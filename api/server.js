1d05929b54916d6c864b324040a62d662187b036
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-03-08 23:56:34 -0500

    oops i forgot to push

--- api/server.js
+++ api/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
+const { logger } = require('./middleware/middleware')
 const server = express();
+const usersRouter = require('./users/users-router')
 
-// remember express by default cannot parse JSON in request bodies
 
-// global middlewares and the user's router need to be connected here
+server.use(express.json())
+server.use(logger)
+server.use('/api/users', usersRouter)
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
 
 module.exports = server;
